97278fb1ff8a276fae09918045057bc6829c49e2
acareaga <aaroncareaga@gmail.com>
2015-11-07 15:09:37 -0700

    UserRewards join table working, rewards displayed on Admin Dashboard. Adding edit and delete functionality

--- app/controllers/admin/rewards_controller.rb
+++ app/controllers/admin/rewards_controller.rb
@@ -1,10 +1,12 @@
 class Admin::RewardsController < Admin::BaseController
 
   def new
+    @user = User.find(params[:format])
     @reward = Reward.new
   end
 
   def create
+    params
     @reward = Reward.new(reward_params)
     if @reward.save
       flash[:notice] = "You successfuly created a reward!"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :user_rewards
+  has_many :rewards, through: :user_rewards
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 
