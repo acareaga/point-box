e54faf5e4ea665f136db90962ffe8d06dfa6944e
ShannonPaige <spaige@gmail.com>
2015-11-08 22:00:25 -0700

    Refactor and Add Model Tests

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -7,22 +7,12 @@ class RewardsController < ApplicationController
   def create
     @user = current_user
     @reward = Reward.find(params[:reward_id])
-    # User.deduct_points(@user.id)
-    # see user.rb
-    if @user.points_available.nil?
-      flash[:error] = "You don't have any points, you can't buy anything!!!"
-    elsif @reward.point_value > @user.points_available
-      flash[:error] = "You don't have enought points for this!!!"
-    else
-      new_points_available = @user.points_available - @reward.point_value
-      new_redeemed_points = @user.points_redeemed + @reward.point_value
-      @user.update_attributes!(points_available: new_points_available)
-      @user.update_attributes!(points_redeemed: new_redeemed_points)
-      @user.rewards << @reward
-      @user.save
+    error_message = User.buy_reward(@user, @reward)
+    if error_message.nil?
       flash[:notice] = "You bought it!!!"
+    else
+      flash[:error] = error_message
     end
-
     redirect_to user_path(@user)
   end
 end
--- test/models/reward_test.rb
+++ test/models/reward_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class RewardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def valid_attributes
+    {
+      name: "Paris",
+      point_value: 500
+    }
+  end
+
+  test "reward is created with valid attributes" do
+    reward = Reward.new(valid_attributes)
+
+    assert reward.valid?
+    assert_equal "Paris", reward.name
+    assert_equal 500, reward.point_value
+  end
+
+  test "reward is not created when missing name" do
+    reward = Reward.new(valid_attributes)
+    reward.name = nil
+
+    assert reward.invalid?
+  end
+
+  test "reward is not created when missing point value" do
+    reward = Reward.new(valid_attributes)
+    reward.point_value = nil
+
+    assert reward.invalid?
+  end
 end
